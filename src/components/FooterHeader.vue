c9e85ef7d04bb7341ecbc64f147d22e3872d7cd9
isaac545454 <isaac.newton1221@gmail.com>
2023-03-16 06:59:52 -0300

    feat: files header and footer

---
+++ src/components/FooterHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="footer">
+    <p>f</p>
+  </div>
+</template>
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    msg: String,
+  },
+})
+export default class Footer extends Vue {
+  msg!: string;
+}
+</script>
+  <style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+</style>
+  
